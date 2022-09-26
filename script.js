2a8d8cb2e1cd7164bc074fa19704e2d0b4810a85
sesepopo <114157832+sesepopo@users.noreply.github.com>
2022-09-26 21:50:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,20 +10,17 @@ function gatherAllText() {
   radio = document.getElementsByName('maker');
   if(radio[0].checked) {
       var pointTitle = "この行動の良かった点";
-      var points = Array.prototype.slice.call(document.getElementsByName("goodPoint"));
-      points = filterEmptyText(points);
+      var points = alignNotEmptyElements(document.getElementsByName("goodPoint"));
       
       var othersTitle = "この行動をやっていなかった世界線";
-      var others = Array.prototype.slice.call(document.getElementsByName("anotherStory"));
-      others = filterEmptyText(others);
+      var others = alignNotEmptyElements(document.getElementsByName("anotherStory"));
+
   }else if(radio[1].checked) {
       var pointTitle = "この行動のまずかった点";
-      var points = Array.prototype.slice.call(document.getElementsByName("badPoint"));
-      points = filterEmptyText(points);
+      var points = alignNotEmptyElements(document.getElementsByName("badPoint"));
   
       var othersTitle = "行動の改善案";
-      var others = Array.prototype.slice.call(document.getElementsByName("improvementPlan"));
-      others = filterEmptyText(others);
+      var others = alignNotEmptyElements(document.getElementsByName("improvementPlan"));
   }
   const l_circumstance = "\* 行動の背景説明";
   const l_who = `|誰が|${who}|`;
@@ -34,13 +31,13 @@ function gatherAllText() {
   const l_result = `|結果どうなった|${result}|`;
 
   const l_pointTitle = `\* ${pointTitle}`;
-  const l_points = gatherItems(points);
+  const l_points = points;
 
   const l_othersTitle = `\* ${othersTitle}`;
-  const l_others = gatherItems(others);
+  const l_others = others;
 
-  const l_appendixTitle = `\* ${document.getElementById("appendix").placeholder}`;
-  const l_appendix = "";
+  const l_appendixTitle = `\* ${document.getElementById("appendix1").placeholder}`;
+  const l_appendix = alignNotEmptyElements(document.getElementsByName("appendix"));
 
   const endText = "//必要に応じて丸括弧内を書き換えて記事にタグをつける。&tag(生活);とか\n&tag(test tag);\n//コメント欄を生成\n#pcomment";
 
@@ -49,13 +46,26 @@ function gatherAllText() {
   
   return res;
 }
-function gatherItems(items) {
+
+function itemizeTextsOnWiki(items) {
   var tmp = "";
   items.forEach( function( item ) {
       tmp = `${tmp}-${item.value}\n`;
   });
   return tmp;
 }
+
+function filterEmptyText(items){
+  var res = items.filter(word => word.value.length > 0);
+  return res;
+}
+
+function alignNotEmptyElements(items) {
+  var items_array = Array.prototype.slice.call(items);
+  items_array = filterEmptyText(items_array);
+  return itemizeTextsOnWiki(items_array);
+}
+
 function copy2Clipboard() {
   var copyTarget = gatherAllText();
   navigator.clipboard.writeText(copyTarget).then(success,faild);
@@ -69,10 +79,6 @@ function copy2Clipboard() {
   
 }
 
-function filterEmptyText(items){
-  var res = items.filter(word => word.value.length > 0);
-  return res;
-}
 
 function formSwitch(){
   radio = document.getElementsByName('maker');
@@ -86,3 +92,33 @@ function formSwitch(){
 }
 
 window.onload = formSwitch();
+
+var i = 1 ;
+function addForm(name, parentId) {
+  var input_data = document.createElement('textarea');
+  input_data.className = 'large';
+  input_data.id = 'inputform_' + i;
+  input_data.name = name;
+  input_data.placeholder = '追加フォーム-' + i;
+  // var parent = document.getElementById('form_area');
+  var parent = document.getElementById('form_area_' + parentId);
+  parent.appendChild(input_data);
+
+  var button_data = document.createElement('button');
+  button_data.id = i;
+  button_data.onclick = function(){deleteBtn(this, parentId);}
+  button_data.innerHTML = '削除';
+  var input_area = document.getElementById(input_data.id);
+  parent.appendChild(button_data);
+
+  i++ ;
+}
+
+function deleteBtn(target, parentId) {
+  var target_id = target.id;
+  var parent = document.getElementById('form_area_' + parentId);
+  var ipt_id = document.getElementById('inputform_' + target_id);
+  var tgt_id = document.getElementById(target_id);
+  parent.removeChild(ipt_id);
+  parent.removeChild(tgt_id);	
+}
